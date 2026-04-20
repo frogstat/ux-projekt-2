b04060c890240dc74bec462d3c7471e908dc0cd2
frogstat <frogstatftw@gmail.com>
2026-04-20 09:33:17 +0200

    Made filter button work

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ budgetInput.addEventListener('blur', () => {
 budgetInput.addEventListener('focus', () => {
     budgetInput.value = budgetInput.value.replace(' kr', '');
 });
+
+const filterButton = document.getElementsByClassName("close-filter")[0];
+const filterIcon = document.getElementById("filter-icon");
+const filterSettings = document.getElementsByClassName("filter")[0];
+let isFilter = false;
+filterSettings.style.display = 'none';
+filterIcon.src = isFilter ? 'x.png' : 'filter.png';
+
+filterButton.addEventListener('click', () => {
+    isFilter = !isFilter;
+    filterSettings.style.display = filterSettings.style.display === 'none' ? 'block' : 'none';
+    filterIcon.src = isFilter ? 'x.png' : 'filter.png';
+});
